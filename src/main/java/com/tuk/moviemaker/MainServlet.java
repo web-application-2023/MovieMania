5d1246d4a8b7479b15943e3c834cd305050a13b5
JeongHunHui <deca3248@gmail.com>
2023-12-03 16:24:44 +0900

    feat: 세션에서 name을 가져와서 있으면 로그인, 없으면 로그인 안됨으로 판단

--- src/main/java/com/tuk/moviemaker/MainServlet.java
+++ src/main/java/com/tuk/moviemaker/MainServlet.java
@@ -4,6 +4,7 @@ import com.tuk.moviemaker.movie.dao.MovieDao;
 import com.tuk.moviemaker.movie.entity.Movie;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.print.attribute.standard.JobOriginatingUserName;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/main")
 public class MainServlet extends HttpServlet {
@@ -44,9 +46,14 @@ public class MainServlet extends HttpServlet {
         long totalMovies = movieDao.countMovies();
         int totalPages = (int) Math.ceil((double) totalMovies / pageSize);
 
+        HttpSession session = request.getSession();
+        Boolean isLogin = session.getAttribute("user_name") != null;
+
         request.setAttribute("movies", movies);
         request.setAttribute("currentPage", page);
         request.setAttribute("totalPages", totalPages);
+        request.setAttribute("isLogin", isLogin);
+
 
         request.getRequestDispatcher("/mainPage.jsp").forward(request, response);
     }
