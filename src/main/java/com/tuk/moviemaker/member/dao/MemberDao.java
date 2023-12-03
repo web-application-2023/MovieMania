95405d165d8a807709c4af87572d5371d70eaf3f
조성현 <agllwy116@naver.com>
2023-12-03 14:03:47 +0900

    email로 DB에서 맞는 회원 찾는 메서드 생성

--- src/main/java/com/tuk/moviemaker/member/dao/MemberDao.java
+++ src/main/java/com/tuk/moviemaker/member/dao/MemberDao.java
@@ -2,6 +2,8 @@ package com.tuk.moviemaker.member.dao;
 
 import com.tuk.moviemaker.member.entity.Member;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
 public class MemberDao {
     private EntityManager em;
@@ -12,6 +14,16 @@ public class MemberDao {
 
     public Member findById(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public Member findByEmail(String email) {
+        TypedQuery<Member> query = em.createQuery("SELECT m FROM Member m WHERE m.email = :email", Member.class);
+        query.setParameter("email", email);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null; // 해당 이메일에 대한 Member를 찾을 수 없을 경우 null 반환
+        }
     }
 
     public void save(Member member) {
